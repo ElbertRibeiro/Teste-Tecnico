2db7ec0b139dabe5c85fc0e9b628ad216f8179a6
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2022-03-02 17:36:34 -0300

    Update index.js

--- src/homePage/index.js
+++ src/homePage/index.js
@@ -1,9 +1,7 @@
 import ChatBot from 'react-simple-chatbot';
-//import api from '../api/axios';
 import steps from '../components/Chatbot';
 
 export default function Chat() {
-
   return (
     <div>
       <ChatBot steps={steps}/>
